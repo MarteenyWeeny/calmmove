85a65fe60ae98a0f8fb5e5203b5f0d3249870c44
MarteenyWeeny <martin9876543219@gmail.com>
2026-03-03 00:13:53 +0800

    [pages] Add initial Vue application routes

---
+++ pages/support.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useSeoMeta, useHead } from '@vueuse/head';
+
+const title = "Calm Move | Home";
+const description = "Smart Pantry";
+
+useSeoMeta({
+    title: () => title,
+    description: () => description,
+    charset: "utf-8",
+    viewport: "width=device-width, initial-scale=1.0"
+});
+
+useHead({
+    link: [
+        {rel: 'icon', type: 'image/png', href: '../public/logo.png'},
+        {rel: 'stylesheet', href: '../public/reset.css'}
+    ]
+});
+</script>
+
+<template>
+    <client-only>
+        <NavBar />
+        <Welcome />
+        <Copyright />
+    </client-only>
+</template>
+
+<style scoped></style>
+<style></style>
+
