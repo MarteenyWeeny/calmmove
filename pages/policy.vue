fb1d804ac53c9aedc18b8213526357689d0b54dc
MarteenyWeeny <martin9876543219@gmail.com>
2026-03-11 21:26:27 +0800

    [pages] edit 'policy.vue' to add fonts

--- pages/policy.vue
+++ pages/policy.vue
@@ -1,7 +1,17 @@
+<script lang="ts"></script>
+
+<template>
+    <client-only>
+        <!-- <NavBar /> -->
+        <div id="CMMain" class="CM-main"></div>
+        <!-- <Copyright /> -->
+    </client-only>
+</template>
+
 <script setup lang="ts">
 
-const title = "Calm Move | Home";
-const description = "Smart Pantry";
+const title = "LetMCook | Home";
+const description = "Smart meal planner using your ingredients to meet nutrient goals.";
 
 useSeoMeta({
     title: () => title,
@@ -12,19 +22,16 @@ useSeoMeta({
 
 useHead({
     link: [
-        { rel: 'icon', type: 'image/png', href: '/logo.png' },
-        { rel: 'stylesheet', href: '/reset.css' }
+        {rel: 'icon', type: 'image/png', href: '/logo.png'},
+        {rel: 'stylesheet', href: '/reset.css'},
+        {rel: 'stylesheet', href: '/custom.css'} ,
+        {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+        {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''}
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap'}
     ]
 });
+
 </script>
-
-<template>
-    <client-only>
-        <NavBar />
-        <Welcome />
-        <Copyright />
-    </client-only>
-</template>
-
 <style scoped></style>
 <style></style>
+
