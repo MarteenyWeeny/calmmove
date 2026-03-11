3b2a84a69731c75e947b98beb66cb6628957575f
MarteenyWeeny <martin9876543219@gmail.com>
2026-03-11 21:48:10 +0800

    [UI] Add NavBar component with design and usage

---
+++ components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <client-only>
+        <header class="CM-navbar">
+            <nav class="CM-nav-left">
+                <a href="/" class="CM-nav-home">
+                    <img src="/logo.png" width=20 height=20 class="CM-nav-logo">
+                    <span>CalmMove</span>
+                </a>
+            </nav>
+            <nav class="CM-nav-right">
+                <ul>
+                    <li><a href="#" class="CM-nav-link">User Registers and Sets Nutrition Goals</a></li>
+                    <li><a href="#" class="CM-nav-link">User Adds Ingredients to Pantry</a></li>
+                    <li><a href="#" class="CM-nav-link">App Suggests Recipes Based on Pantry Ingredients</a></li>
+                </ul>
+            </nav>
+        </header>
+    </client-only>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 
 <template>
     <client-only>
-        <!-- <NavBar /> -->
+        <NavBar />
         <div id="CMMain" class="CM-main"></div>
         <!-- <Cookies /> -->
         <!-- <Copyright /> -->
@@ -28,7 +28,7 @@ useHead({
         {rel: 'stylesheet', href: '/custom.css'} ,
         {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
         {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''}
-        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap'}
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap'},
     ]
 });
 
