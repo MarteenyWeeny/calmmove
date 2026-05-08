e37d2ba1db4f9fdcbe0625c62591aececafa98e5
Geryme M. Vega <24-1-01154@vsu.edu.ph>
2026-05-08 17:38:34 +0800

    [feat] Add SignIn.vue component and signin.vue page

---
+++ components/SignIn.vue
@@ -0,0 +1,32 @@
+<template>
+  <client-only>
+    <div id="CMSignInBox" class="CM-web-box CM-hidden">
+      <div class="CM-web-header">Sign In</div>
+      <div class="CM-web-slogan">Make the world cleaner.</div>
+      <div id="CMSignInForm" class="CM-web-form">
+        <input
+          id="CMSignInEmail"
+          name="CMSignInEmail"
+          type="text"
+          class="CM-web-input"
+          placeholder="Email"
+          required
+        />
+        <input
+          id="CMSignInPassword"
+          name="CMSignInPassword"
+          type="password"
+          class="CM-web-input"
+          placeholder="Password"
+          required
+        />
+      </div>
+      <div id="CMSignInMessage" class="CM-web-message CM-hidden">
+        Test message...
+      </div>
+      <div id="CMSignInActions" class="CM-web-actions">
+        <div id="CMSignInOK" class="CM-web-action CM-web-ok">Sign In</div>
+      </div>
+    </div>
+  </client-only>
+</template>
